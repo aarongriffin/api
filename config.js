20b2fca34c0c18ee467756c48c75a6c4414fcad7
aarongriffin <aarongriffin@users.noreply.github.com>
2017-03-09 10:17:54 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
     name: 'API',
     version: '0.0.1',
-    aws-id: process.env AWS_ACCESS_KEY_ID || 'AKIAJKOCUSWZ6FM33R7A',
-    aws-key:process.env AWS_SECRET_ACCESS_KEY || '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',
+    aws-id: process.env AWS_ACCESS_KEY_ID || 'xxxxxxxx',
+    aws-key:process.env AWS_SECRET_ACCESS_KEY || 'xxxxxxxx',
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || 'http://localhost:3000',
